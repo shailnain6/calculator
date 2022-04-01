d3ebae106abe625c50d3665a1ec3b5e2f52a5ab8
Shailendra Nain <shailendranain@ShailendrasiMac.lan>
2022-04-01 14:57:24 -0400

    Initial Calculater UI is completed

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import {ACTIONS} from '../App';
+
+const Button = (props) =>{
+    return<>
+        <button className={props.className}>{props.children}</button>
+    </>
+}
+
+export default Button;
